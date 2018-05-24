858e74f77f6c3aa2a980a8a2a9c2ea909f1bcc8e
Emily Jirles <emily.jirles@gmail.com>
2018-05-24 22:13:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ class App < Sinatra::Base
   end 
   
   get '/hometown' do 
-    "My hometown is "
+    "My hometown is Lore City, OH"
   end 
   
   get '/favorite-song' do 
-    "My favorite song is "
+    "My favorite song is Ends of the Earth by Lord Huron"
   end 
   
 end
